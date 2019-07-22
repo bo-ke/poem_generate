e310f070798eb1f7cf80630fc20f47c41046c93c
kebo <itachi971009@gmail.com>
2019-07-23 00:54:45 +0800

    假接口编写

---
+++ api_fake.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+@author: kebo
+@contact: itachi971009@gmail.com
+
+@version: 1.0
+@file: api_fake.py
+@time: 2019-07-23 00:48
+
+这一行开始写关于本文件的说明与解释
+"""
+def match_name(name1):
+    """
+
+    :param name1: 输入的名字
+    :return:
+    """
+    return "name2"
+
+
+def generate_poem(name1,name2):
+    """
+
+    :param name1:
+    :param name2: 匹配的名字
+    :return: 藏头诗
+    """
+    return "poem"
+
+def get_photo(name1,name2):
+    """
+
+    :param name1:
+    :param name2:
+    :return: 生成的合照
+    """
+    return "photo.jpg"
+
+def make_name(name1,name2):
+    """
+
+    :param name1:
+    :param name2:
+    :return:  起的孩子的名字
+    """
+    return "name3"
